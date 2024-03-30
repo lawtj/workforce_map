4e87477537932c3759419e0575b400562fca5cac
tyler-law <tyler.law@ucsf.edu>
2024-03-30 10:49:51 -0700

    add subunits to canesca

--- pages/2_CANESCA.py
+++ pages/2_CANESCA.py
@@ -10,20 +10,33 @@ from streamlit_extras.row import row
 st.set_page_config(page_title='CANESCA', page_icon=':earth_americas:', layout='wide', initial_sidebar_state='collapsed')
 
 @st.cache_data
-def read_gdf():
-    gdf = gpd.read_file('data/geodf.geojson')
-    return gdf
+def read_country():
+    country = gpd.read_file('data/can_country.geojson')
+    return country
+
+@st.cache_data
+def read_states():
+    states = gpd.read_file('data/can_states.geojson')
+    return states
 
 @st.cache_data
 def read_csv():
     can = pd.read_csv('data/canesca.csv')
     return can
 
+country = read_country()
+states = read_states()
+can = read_csv()
+
 def canescamap():
+    country = read_country()
+    states = read_states()
+    can = read_csv()
+
     m = folium.Map(location=[-10, 30], zoom_start=4.5)
 
     attr = 'Tiles Courtesy of Stadia'
-    tiles = 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?api_key='+st.secrets['stadiamaps']
+    tiles = 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?'+st.secrets['stadiamaps']
 
     tile_layer = folium.TileLayer(
     tiles=tiles,
@@ -31,22 +44,27 @@ def canescamap():
     name='Jawg Light',
     overlay=False,
     control=False,
+    opacity=0.5
 
     ).add_to(m)
 
+    countries = folium.GeoJson(country, name='countries',
+                               style_function=lambda x: {'fillColor': 'green', 'color': 'black', 'weight': 2, 'fillOpacity': 0.00, "dashArray": "5, 5"},
+                               ).add_to(m)
+
+    boundaries = folium.GeoJson(states, name='boundaries',
+                                style_function=lambda x: {'fillColor': 'blue', 'color': 'purple', 'weight': 0, 'fillOpacity': 0.05,},
+                                highlight_function=lambda x: {'weight': 1, 'fillOpacity': 0.1},
+                                tooltip=folium.features.GeoJsonTooltip(fields=['admin','name'], aliases=['Country: ', 'District: '])
+                                ).add_to(m)
+        
 
     marker_cluster = MarkerCluster().add_to(m)
-
-    can = read_csv()
-
-
-
-    # folium.GeoJson(can).add_to(m)
 
     for i in can['Longitude'].unique():
         if pd.notnull(i):
             folium.Marker(location=[can[can['Longitude'] == i]['Latitude'].values[0], i], 
-                          tooltip=folium.Tooltip(can[can['Longitude']==i]['PRIMARY_ORGANISATION'].values[0])
+                          tooltip=folium.Tooltip('<strong>'+str(can[can['Longitude']==i]['PRIMARY_ORGANISATION'].values[0])+'</strong>: <p>'+str(can[can['Longitude']==i]['Number_of_Anaesthesiologists'].values[0])+' anaesthesiologist(s)'),
                           ).add_to(marker_cluster)
     return m
 
@@ -62,7 +80,4 @@ row1.page_link('pages/2_CANESCA.py',label='Anesthesiologists in East, Central an
 st.title('Anesthesiologists in East, Central and Southern Africa')
 
 
-st_folium(canescamap(),use_container_width=True)
-
-# st.dataframe(pd.DataFrame(read_gdf()))
-# st.write(read_csv())
+st_folium(canescamap(),use_container_width=True, returned_objects=[])
