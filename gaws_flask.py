70d96bb2373c58f30524ff71f68e517bd15ba94c
tyler-law <tyler.law@ucsf.edu>
2024-04-13 10:50:48 -0700

    variable color scale

--- gaws_flask.py
+++ gaws_flask.py
@@ -2,6 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+sequential_color_schemes = [
+    "Blues",
+    "Greens",
+    "Greys",
+    "Oranges",
+    "Purples",
+    "Reds",
+    "Blue to Green (BuGn)",
+    "Blue to Purple (BuPu)",
+    "Green to Blue (GnBu)",
+    "Orange to Red (OrRd)",
+    "Purple to Blue to Green (PuBuGn)",
+    "Purple to Blue (PuBu)",
+    "Purple to Red (PuRd)",
+    "Red to Purple (RdPu)",
+    "Yellow to Green to Blue (YlGnBu)",
+    "Yellow to Green (YlGn)",
+    "Yellow to Orange to Brown (YlOrBr)",
+    "Yellow to Orange to Red (YlOrRd)"
+]
+
 
 iframe_routes = ['gaws_iframe', 'canesca_iframe', 'ohns_iframe']
 @app.route("/<iframe_name>")
@@ -31,9 +52,9 @@ def CANESCA():
 def OHNS():
     return render_template("pages/ohns.html")
 
-@app.route("/alpine")
+@app.route("/ohns_colors")
 def folium():
-    return render_template("pages/alpinetest.html")
+    return render_template("pages/ohnsv2.html", sequential_color_schemes=sequential_color_schemes)
 
 if __name__ == "__main__":
     app.run(debug=True)
